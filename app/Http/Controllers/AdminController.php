eb8268b1d8d8b18c679ba0de6b76e426548aeb1b
LesMiserables1 <liemandre20@gmail.com>
2019-08-26 21:13:51 +0700

    delete foto dan video

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -103,4 +103,60 @@ class AdminController extends Controller
         }
         return view('approve')->with('photos',$arrPhoto)->with('videos',$arrVideo);
     }
+    public function showEdit()
+    {
+        $photos = Photo::where('approve',true)->get();
+        $prevCaption = "";
+        $x = 0;
+        $arrPhoto = array(array());
+        $visit = [];
+        for($k = 0;$k<count($photos) ; $k++){
+            $caption = $photos[$k]->caption;
+            $visit[$k] = $visit[$k]??false;
+            if($prevCaption != $caption && !$visit[$k]){
+                $sortedPhoto = [];
+                $i = 0;
+                $j = 0;        
+                foreach($photos as $foto){
+                    if($caption == $foto->caption){
+                        $sortedPhoto[$j++] = $foto;
+                        $visit[$i] = true;
+                    }
+                    $i++;
+                }
+                $arrPhoto[$x]['caption'] = $caption;
+                $arrPhoto[$x]['foto'] = $sortedPhoto;
+                $x++;
+                $prevCaption = $caption;
+            }
+        }
+        
+        $videos = Video::where('approve',true)->get();
+        $prevCaption = "";
+        $x = 0;
+        $arrVideo = array(array());
+        $visit = array();
+        for($k = 0;$k<count($videos) ; $k++){
+            $caption = $videos[$k]->caption;
+            $visit[$k] = $visit[$k]??false;
+            if($prevCaption != $caption && !$visit[$k]){
+                $sortedVideo = [];
+                $i = 0;
+                $j = 0;        
+                foreach($videos as $video){
+                    if($caption == $video->caption){
+                        $sortedVideo[$j++] = $video;
+                        $visit[$i] = true;
+                    }
+                    $i++;
+                }
+                $arrVideo[$x]['caption'] = $caption;
+                $arrVideo[$x]['video'] = $sortedVideo;
+                $x++;
+                $prevCaption = $caption;
+            }
+        }
+        return view('delete')->with('photos',$arrPhoto)->with('videos',$arrVideo);
+    }
+    
 }
