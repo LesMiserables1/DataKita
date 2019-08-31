eb0db6a303c172d892987ac7f2ed9f3cbbc3818c
rayhannr <rayhannaufal2017@mail.ugm.ac.id>
2019-08-31 14:25:07 +0700

    update dikit approve

--- resources/views/approve.blade.php
+++ resources/views/approve.blade.php
@@ -1,30 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Approve</title>
+    <link rel="stylesheet" href="{!! asset('css/general.css') !!}">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css">
+
+    <!-- default passive events for performance optimization -->
+    <script src="https://cdn.jsdelivr.net/npm/default-passive-events@1.0.10/dist/index.min.js"></script>
+</head>
 <body>
-<form action="/admin/approve" method="post">
-@csrf
-@if(count($photos[0])!=0)
-    @foreach($photos as $photo)
-    {{$photo['caption']}}
-    @foreach($photo['foto'] as $foto)
-    <img src="/show/foto/{{$foto['nama_foto']}}">
-    <input type="checkbox" name="check[]" value="{{$foto['id']}} " id="">
-    @endforeach
-    @endforeach
-@endif
-@if(count($videos[0])!=0)
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-md navbar-dark" id="navbar">
+        <a class="navbar-brand" href="/">Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div id="nav-mhweb-hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item mr-lg-4 mr-0 mr-md-1">
+                    <!-- link sini -->
+                    <a class="nav-link" href="/"><i class="fa fa-home" aria-hidden="true"></i> Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown mr-lg-4 mr-0 mr-md-1">
+                    <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fa fa-image" aria-hidden="true"></i> Galleries
+                    </a>
+                    <!-- link sini -->
+                    <ul class="dropdown-menu ml-0 mr-0" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="/Agriculture-Mining">Agriculture & Mining</a></li>
+                        <li><a class="dropdown-item" href="/Social-Population">Social & Population</a></li>
+                        <li><a class="dropdown-item" href="/Economic-Trade">Economic & Trade</a></li>
+                        <li><a class="dropdown-item active" href="/Events-Experiences">Events & Experiences</a></li>
+                        <li><a class="dropdown-item" href="/Infographics">Infographics</a></li>
+                    </ul>
+                </li>
+                <!-- link sini, tapi belum ada pagenya -->
+                <li class="nav-item mr-lg-4 mr-0 mr-md-1">
+                    <a class="nav-link" href="/about"><i class="fa fa-address-card" aria-hidden="true"></i> About</a>
+                </li>
+                <li class="nav-item dropdown mr-lg-4 mr-0 mr-md-1">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-upload"></i> Upload
+                    </a>
+                    <ul class="dropdown-menu ml-0 mr-0" aria-labelledby="navbarDropdown2">
+                        <li><a class="dropdown-item" href="/upload/photo">Photo</a></li>
+                        <li><a class="dropdown-item" href="/upload/video">Video</a></li>
+                    </ul>
+                </li>
+                @if(isset(Auth::user()->id))
+                <li class="nav-item dropdown mr-lg-4 mr-0 mr-md-1">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown2" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-upload"></i> Admin
+                    </a>
+                    <ul class="dropdown-menu ml-0 mr-0" aria-labelledby="navbarDropdown2">
+                        <li><a class="dropdown-item" href="/admin/approve">Approve</a></li>
+                        <li><a class="dropdown-item" href="/admin/delete">Delete</a></li>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                      document.getElementById('logout-form').submit();">
+                         {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                         @csrf
+                        </form>
+                    </ul>
+                </li>
+                @endif
+            </ul>
+            <form class="form-inline my-2 my-lg-0" method="get" action="/search">
+                <input class="form-control search-box mr-2" type="search" placeholder="Search" aria-label="Search" name="search_query">
+                <button class="btn btn-primary my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+            </form>
+        </div>
+    </nav>
+
+    <form action="/admin/approve" method="post">
     @csrf
-    @foreach($videos as $video)
-        {{$video['caption']}}
-        @foreach($video['video'] as $vid)
-            <!-- <iframe width="640" height="390" src="http://www.youtube.com/embed/{{$vid->video_id}}?enablejsapi=1" frameborder="0" allowfullscreen></iframe> -->
-            <div class="codegena_iframe" data-src="http://www.youtube.com/embed/{{$vid->video_id}}?enablejsapi=1" data-img="https://img.youtube.com/vi/{{$video->video_id}}/sddefault.jpg" data-responsive="true"></div>    
-            <input type="checkbox" name="checkvideo[]" value="{{$vid['id']}}">
+    @if(count($photos[0])!=0)
+        @foreach($photos as $photo)
+        {{$photo['caption']}}
+        @foreach($photo['foto'] as $foto)
+        <img src="/show/foto/{{$foto['nama_foto']}}">
+        <input type="checkbox" name="check[]" value="{{$foto['id']}} " id="">
         @endforeach
-    @endforeach
-    <button type="submit" value="approve" name="status">Approve</button>
-    <button type="submit" value="delete" name="status">Delete</button>
+        @endforeach
+    @endif
+    @if(count($videos[0])!=0)
+        @csrf
+        @foreach($videos as $video)
+            {{$video['caption']}}
+            @foreach($video['video'] as $vid)
+                <!-- <iframe width="640" height="390" src="http://www.youtube.com/embed/{{$vid->video_id}}?enablejsapi=1" frameborder="0" allowfullscreen></iframe> -->
+                <div class="codegena_iframe" data-src="http://www.youtube.com/embed/{{$vid->video_id}}?enablejsapi=1" data-img="https://img.youtube.com/vi/{{$video->video_id}}/sddefault.jpg" data-responsive="true"></div>    
+                <input type="checkbox" name="checkvideo[]" value="{{$vid['id']}}">
+            @endforeach
+        @endforeach
+        @endif
+        <button class="btn btn-outline-primary" type="submit" value="approve" name="status">Approve</button>
+        <button class="btn btn-outline-danger" type="submit" value="delete" name="status">Delete</button>
 
-</form>
+    </form>
 
-<script src="{!! asset('js/async-iframe.js') !!}"></script>
+    <script src="{!! asset('js/async-iframe.js') !!}"></script>
+    <script src="{!! asset('js/header.js') !!}"></script>
 </body>
-@endif
+
+</html>
